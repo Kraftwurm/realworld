481f29a4ad951405d1db7b732bc09cc6e0a6ae12
Kraftwurm <kraftwurm@gmail.com>
2022-04-22 16:13:44 +0200

    cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
-import netlify from '@sveltejs/adapter-netlify';
-import vercel from '@sveltejs/adapter-vercel';
+//import netlify from '@sveltejs/adapter-netlify';
+//import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 export default {
 	kit: {
-		adapter: process.env.VERCEL ? vercel() : netlify()
+		adapter: adapter()
 	}
 };

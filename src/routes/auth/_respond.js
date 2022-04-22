31d48c9391f0262e87e901fa1956837e83bc7b2b
Kraftwurm <kraftwurm@gmail.com>
2022-04-22 17:20:06 +0200

    test

--- src/routes/auth/_respond.js
+++ src/routes/auth/_respond.js
@@ -1,10 +1,21 @@
+function bytes2hex(bytes) {
+	return Array.prototype.map.call(bytes,
+		byte => ('0' + byte.toString(16)).slice(-2)).join('');
+}
+
 export function respond(body) {
 	if (body.errors) {
 		return { status: 401, body };
 	}
 
 	const json = JSON.stringify(body.user);
-	const value = Buffer.from(json).toString('base64');
+	
+	// Buffer.from is not available in Cloudflare workers
+	// const value = Buffer.from(json).toString('base64');
+
+	let bytes = new TextEncoder().encode(json)
+	const value = bytes2hex(bytes)
+
 
 	return {
 		headers: {
